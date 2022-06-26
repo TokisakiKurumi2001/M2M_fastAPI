200d52e84eda42bee016a47b8e4ec0cbfdef11d2
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2022-06-26 20:39:23 +0700

    More features for API

--- api.py
+++ api.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import os
+import time
+from starlette.background import BackgroundTask
+
 
 from pipeline import TranslationPipeline
 
@@ -22,7 +27,7 @@ app.add_middleware(
 )
 
 
-@app.post("/translate")
+@app.post("/translate/text")
 async def translate(translation: TranslationItem):
     print(f"Khoi_M2M, {translation.text[0:50]}")
     text = translation.text
@@ -34,4 +39,37 @@ async def translate(translation: TranslationItem):
             'src': vi_paragraphs,
             'tgt': ba_paragraphs
         }
+    }
+
+
+@app.post("/translate/file")
+async def translateFile(file: UploadFile = File(...), model: str = Form(...)):
+    content = await file.read()
+    text = content.decode('utf-8')
+    # translated_text, model_type = await pipeline(text, model)
+    _, ba_paragraphs = await pipeline(text)
+
+    tmp_dir = 'tmp_files'
+    isExist = os.path.exists(tmp_dir)
+    if not isExist:
+        os.makedirs(tmp_dir)
+        print(f'Directory "{tmp_dir}" is created!')
+
+    filename = str(int(time.time())) + file.filename
+    with open(f'{tmp_dir}/{filename}', encoding='utf-8', mode='w') as f:
+        f.write('\n'.join(ba_paragraphs))
+
+    def cleanup(filename):
+        os.remove(f'{tmp_dir}/{filename}')
+
+    return FileResponse(
+        f'{tmp_dir}/{filename}',
+        background=BackgroundTask(cleanup, filename),
+    )
+
+
+@app.get("/models")
+async def getModels():
+    return {
+        'models': ['M2M']
     }
